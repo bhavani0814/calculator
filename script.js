bcacbea9a3829fc091bda9b6b0eb15bf4dc2eff4
bhavani0814 <102813360+bhavani0814@users.noreply.github.com>
2023-09-05 09:01:43 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ <script>
+            function ac(){
+                document.getElementById("res").value="";
+            }
+            function show(input){
+                document.getElementById("res").value+=input;
+            }
+            function cal(){
+                var output=eval(document.getElementById("res").value);
+                document.getElementById("res").value=output;
+            }
+ </script>
